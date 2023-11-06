e49c8a0248a277193767b08b96e38c8f37b2b9e7
Petar Paunov <peter.paunov20@gmail.com>
2023-11-06 13:55:40 +0200

    Auth controller prepared for initialization.

---
+++ src/Microservices/DevBlog.Service.AuthAPI/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿namespace DevBlog.Service.AuthAPI.Controllers
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> Register()
+        {
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login()
+        {
+            return Ok();
+        }
+    }
+}
