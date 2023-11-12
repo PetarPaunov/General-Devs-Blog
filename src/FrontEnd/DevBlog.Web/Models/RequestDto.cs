e3f40d6bed48f224334bb19c602817cbcdd7728a
PetarPaunov <petarp.2000@abv.bg>
2023-11-12 18:35:29 +0200

    Added request and response DTO's

---
+++ src/FrontEnd/DevBlog.Web/Models/RequestDto.cs
@@ -0,0 +1,29 @@
+﻿namespace DevBlog.Web.Models
+{
+    public class RequestDto
+    {
+        /// <summary>
+        /// The request method.
+        /// Default -> GET.
+        /// </summary>
+        public string RequestType { get; set; } = "GET";
+
+        /// <summary>
+        /// The url to which the request goes.
+        /// </summary>
+        public string Url { get; set; } = null!;
+
+        /// <summary>
+        /// The data sent with the request.
+        /// Leaving it an Object so it can be used with any data type.
+        /// </summary>
+        public object Data { get; set; } = null!;
+
+        /// <summary>
+        /// User authorization token.
+        /// </summary>
+
+        public string AccessToken { get; set; } = null!;
+
+    }
+}
---
+++ src/FrontEnd/DevBlog.Web/Models/ResponseDto.cs
@@ -0,0 +1,17 @@
+﻿namespace DevBlog.Web.Models
+{
+    public class ResponseDto
+    {
+        /// <summary>
+        /// Information returned by the service. 
+        /// Leaving it an Object so it can be used as a generic response DTO.
+        /// </summary>
+        public object? Result { get; set; }
+
+        // Indicates whether the request was successful.
+        public bool IsSuccess { get; set; }
+
+        // Information about why the request failed.
+        public string Message { get; set; } = null!;
+    }
+}
